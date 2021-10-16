5e19a12d9af7102d2fa006f36813c4df2e5610dd
EASHOVON <ashrafuzzaman.shovon.786@gmail.com>
2021-10-16 16:12:33 +0600

    Header component Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Header from './Header';
 
 function App()
 {
   return (
+    // BEM
     <div className="App">
-      <h1>Hello Clever Programmers, let's build the Amazone Store :rocket</h1>
+      {/* Header */}
+      <Header></Header>
+      {/* Home */}
     </div>
   );
 }
